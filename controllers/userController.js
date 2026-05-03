b40e94635887fddad22ffeb4f5bce747445fac1a
Rukundo <gihozoRukundobenise@gmail.com>
2026-05-03 21:43:42 +0200

    The users' controller file

---
+++ controllers/userController.js
@@ -0,0 +1,29 @@
+let users = []
+
+const getUsers = (req, res) => {
+    res.send(users)
+}
+
+const addUser = (req, res) => {
+    const name = req.body.name
+    if(!name || name.trim() ==="") {
+        return res.status(400).send("Name is required")
+    }
+    const user = { name : name.trim() }
+    users.push(user)
+
+    res.send(user.name + " is added")
+}
+
+const deleteUser = (req, res) => {
+    const index = req.params.index
+    if(!users[index]) {
+        return res.status(404).send("User not found")
+    }
+    users.splice(index, 1)
+
+    res.send("User deleted")
+}
+
+module.exports = { getUsers, addUser, deleteUser}
+
