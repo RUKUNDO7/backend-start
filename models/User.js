9b913edbd36dd062e9ac68344851a1f9f0ad82fd
Rukundo <gihozoRukundobenise@gmail.com>
2026-05-05 09:09:16 +0200

    Added the password field for signup and login

--- models/User.js
+++ models/User.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    }
+    name: String,
+    password: String     
 })
 
 module.exports = mongoose.model("User", userSchema)
