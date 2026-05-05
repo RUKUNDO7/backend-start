94df5848e34c7d86bf729eeec9e59a5f97b80571
Rukundo <gihozoRukundobenise@gmail.com>
2026-05-05 09:09:44 +0200

    Added the signup and login endpoints

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,12 +5,16 @@ const { //destructuring
     getUsers,
     addUser,
     deleteUser, 
-    testUser
+    testUser,
+    signup,
+    login
 } = require('../controllers/userController')
 
 router.get('/', getUsers)
 router.post('/', addUser)
 router.delete('/:id', deleteUser)
 router.get('/test', testUser)
+router.post('/signup', signup)
+router.post('/login', login)
 
 module.exports = router //This makes the router object available to other files
