0f491049a748b03baa0005094204dcc9ba1822a8
Rukundo <gihozoRukundobenise@gmail.com>
2026-05-07 20:14:38 +0200

    used process.env to avoid hardcoded secrets

--- middleware/auth.js
+++ middleware/auth.js
@@ -14,7 +14,9 @@ const auth = (req, res, next) => {
     }
 
     try {
-        const decoded = jwt.verify(token, "secretkey")
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET )
         req.user = decoded
         next()
     } catch (err) {
