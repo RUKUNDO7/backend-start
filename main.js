a8999f70d81a1798c1bb565b4460282d2454b41f
Rukundo <gihozoRukundobenise@gmail.com>
2026-05-08 09:54:01 +0200

    Added an endpoint to indicate online backend is working

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-require("dotenv").config()  //load .env at the very start
+require('dotenv').config()  //load .env at the very start
 const express = require('express')
 const app = express()
 
 const mongoose = require('mongoose')
 
 mongoose.connect("mongodb://127.0.0.1:27017/backend-start")
-.then(() => console.log("MongoDB connected"))
+.then(() => console.log('MongoDB connected'))
 .catch(err => console.log(err))
 
 const userRoutes = require('./routes/userRoutes')
@@ -14,6 +14,10 @@ app.use(express.json())
 
 app.use('/users', userRoutes)
 
+app.get('/', (req, res) => {
+    res.send('Backend API is working')
+})
+
 app.listen(3000, () => {
-    console.log("Server running on port 3000")
+    console.log('Server running on port 3000')
 })
